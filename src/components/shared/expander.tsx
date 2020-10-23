63cebc05e935188bf51adfd5c836baad314cd523
jmacd229 <jmacd229@uwo.ca>
2020-10-22 23:39:54 -0400

    Continued work on expander component
    Added in animation and changed styling for focused elements. Also added prettier configuration to keep semicolons.

--- src/components/shared/expander.tsx
+++ src/components/shared/expander.tsx
@@ -1,35 +1,55 @@
-import React, { useEffect, createRef } from "react"
+import React, { useEffect, createRef, useState } from "react";
 
-import lottie from "lottie-web"
-import animation from "../../animations/menu.json"
-import "../../styles/shared/expander.scss"
+import lottie, { AnimationDirection } from "lottie-web";
+import animation from "../../animations/menu.json";
+import "../../styles/shared/expander.scss";
 
 interface ExpanderProps {
-    label: String,
+  label: String;
+  maxWidth: Number;
 }
 
 const Expander = (props: ExpanderProps) => {
-  let animationContainer = createRef<HTMLDivElement>()
+  const [isExpanded, setExpandedState] = useState(false);
+  let animationContainer = createRef<HTMLDivElement>();
 
   useEffect(() => {
-    lottie.loadAnimation({
+    const anim = lottie.loadAnimation({
       container: animationContainer.current,
       renderer: "svg",
       loop: false,
       autoplay: false,
       animationData: animation,
-    })
-  }, [])
+    });
+    function expand() {
+      anim.play();
+      anim.setDirection((anim.playDirection * -1) as AnimationDirection);
+      setExpandedState(anim.playDirection === 1);
+    }
+    animationContainer.current.parentElement.parentElement.addEventListener(
+      "click",
+      expand
+    );
+    return () => {
+      anim.destroy();
+    };
+  }, []);
 
   return (
-    <div className="expander" tabIndex={0} >
-        <div className="expander-label">{props.label}</div>
-      <div className="icon mr-2">
-        <div className="animation-container" ref={animationContainer} />
-      </div>
+    <div className={"expander" + (isExpanded ? " expanded" : "")}>
+      <button
+        className="btn p-0 expander-trigger"
+        style={{
+          transform: `translateX(${isExpanded ? props.maxWidth : 0}px)`,
+        }}
+      >
+        <div className="expander-label py-4">{props.label}</div>
+        <div className="icon flex-shrink-0">
+          <div className="animation-container" ref={animationContainer} />
+          </div>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Expander
-
+export default Expander;
