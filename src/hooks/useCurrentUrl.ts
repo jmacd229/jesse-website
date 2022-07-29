db547c747b51288818664f10cfdbfd351dd9ca1b
jmacd229 <jmacd229@uwo.ca>
2022-07-28 21:36:01 -0400

    fix(header): set default state of currentUrl to home

--- src/hooks/useCurrentUrl.ts
+++ src/hooks/useCurrentUrl.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const useCurrentUrl = (): string => {
-  const [currentUrl, setCurrentUrl] = useState('');
+  const [currentUrl, setCurrentUrl] = useState('/');
   useEffect(() => setCurrentUrl(window.location.pathname), []);
   return currentUrl;
 };
